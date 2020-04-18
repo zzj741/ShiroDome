83f369f84dd1ce81c77249e25f1c2e42910b0067
zzj741 <2897207148@qq.com>
2020-04-18 22:41:48 +0800

    新建shiro模块上传github

---
+++ shiro-modle/src/main/java/shirodemo/shiromodle/ShiroModleApplication.java
@@ -0,0 +1,13 @@
+package shirodemo.shiromodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroModleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiroModleApplication.class, args);
+    }
+
+}
